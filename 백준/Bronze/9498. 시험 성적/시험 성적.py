668a4bd71edce70381d33cb8a5becc42e6915478
YunJongJang <wowman0529@gmail.com>
2023-03-22 10:30:51 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+my_score = int(input())
+
+if my_score >= 90:
+    print('A')
+elif my_score >= 80:
+    print('B')
+elif my_score >= 70:
+    print('C')
+elif my_score >= 60:
+    print('D')
+else:
+    print('F')
